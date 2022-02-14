dd6215db618e2d650ae80668661bd5efdfd21418
생이(SangE/남상이) <tkddl5990@gmail.com>
2022-02-14 14:47:20 +0900

    feat: path alias 설정

---
+++ craco.config.js
@@ -0,0 +1,12 @@
+const path = require("path");
+
+module.exports = {
+  webpack: {
+    alias: {
+      "@": path.resolve(__dirname, "src/"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@layouts": path.resolve(__dirname, "src/layouts"),
+    },
+  },
+};
