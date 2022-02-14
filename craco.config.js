e8a80114437220e9008d0cb942ba6b5619ee41d2
생이(SangE/남상이) <tkddl5990@gmail.com>
2022-02-14 14:49:39 +0900

    feat: prettierrc 및 readme 초기화

--- craco.config.js
+++ craco.config.js
@@ -1,12 +1,12 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
   webpack: {
     alias: {
-      "@": path.resolve(__dirname, "src/"),
-      "@hooks": path.resolve(__dirname, "src/hooks"),
-      "@pages": path.resolve(__dirname, "src/pages"),
-      "@layouts": path.resolve(__dirname, "src/layouts"),
+      '@': path.resolve(__dirname, 'src/'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@layouts': path.resolve(__dirname, 'src/layouts'),
     },
   },
 };
